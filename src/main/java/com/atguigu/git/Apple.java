d791363dc54a9bc8158aab49206b79121f6f6ee1
wzdsgl202102 <wzdsgl202102@aliyun.com>
2021-11-04 00:07:23 +0800

    test conflict right

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -2,8 +2,6 @@ package com.atguigu.git;
 
 public class Apple {
 	public static void main(String[] args) {
-		System.out.println("aaaaaa");
-		System.out.println("ccccc");
-		System.out.println("bbbbbb");
+		System.out.println("right");
 	}
 }
